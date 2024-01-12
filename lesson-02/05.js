e72965028c76e2b4fb20d3ae1ac606df41a48028
Veta-s <elsava1999@gmail.com>
2024-01-12 17:06:27 +0300

    5

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,1 +1,10 @@
-// code
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let newPassport = { ...passport };
+newPassport.name = "Ivan";
+
+console.log(passport.name);
+console.log(newPassport.name);
