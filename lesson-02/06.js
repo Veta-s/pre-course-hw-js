11a9761e6948ff99960e6cfc1353fb21af7642f4
Veta-s <elsava1999@gmail.com>
2024-01-15 16:15:49 +0300

    2.6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -1,1 +1,15 @@
-// code
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let copiedPassportWithAddress = JSON.parse(JSON.stringify(passportWithAddress));
+
+copiedPassportWithAddress.address.city = "Bobryisk";
+
+console.log(passportWithAddress.address.city);
+console.log(copiedPassportWithAddress.address.city);
