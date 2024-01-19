d2857b0a07b46d3b0fe924e71590e59f7a86d0e3
Veta-s <elsava1999@gmail.com>
2024-01-19 18:08:25 +0300

    dbg

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,3 +1,3 @@
 const wordsArr = ["яблоко", "банан", "апельсин", "манго", "киви"];
-let orange = (wordsArr.indexOf("апельсин") === -1) ? console.log("Придется поискать в другом магазине…") : console.log("Ура! нашел")
+let orangeFruit = (wordsArr.indexOf("апельсин") === -1) ? console.log("Придется поискать в другом магазине…") : console.log("Ура! нашел")
 
